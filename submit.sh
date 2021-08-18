356abb82073e039b920460638187096253d9bea0
Kai-Chih Tseng <ktseng@cake.fios-router.home>
2021-08-18 11:05:26 -0400

    Your message about the commit

---
+++ submit.sh
@@ -0,0 +1,6 @@
+git init
+git add <folder1> <folder2> <etc.>
+git commit -m "Your message about the commit"
+git remote add origin https://github.com/yourUsername/yourRepository.git
+git push -u origin master
+git push origin master
---
+++ submit2.sh
@@ -0,0 +1,7 @@
+#!bin/sh
+git init
+git add . 
+git commit -m "Your message about the commit"
+git remote add origin https://github.com/kuiper2000/AR_seasonal_prediction.git
+git push -u origin master
+git push origin master  
